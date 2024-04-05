35f21acb112f130d88f7ceb10a315220d1842e85
nakka tarun teja <nakkatarunteja@TARUNS-MACBOOK.bbrouter>
2024-04-05 12:27:37 +0530

    user_update

--- SkinSaviour/settings.py
+++ SkinSaviour/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'SkinSaviour.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Skin-Saviour',
+        'USER' : 'postgres',
+        'PASSWORD':'Aasri@123',
+        'HOST':'localhost',
+        'PORT':'5433'
     }
 }
 
@@ -125,3 +130,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+AUTH_USER_MODEL = 'core.User'
---
+++ core/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from core.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+       list_display = ['id', 'username', 'email', 'is_active', 'created_at']
+       add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+    
+
+    
