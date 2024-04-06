5833530c58081be05b572ac97dd13da542398c64
Mounika Sangana <sssmounika5@gmail.com>
2024-04-07 02:00:12 +0530

    updated

--- static/header/header.js
+++ static/header/header.js
@@ -7,3 +7,16 @@ function myFunction() {
     document.getElementById("navbar").style.backgroundColor = "transparent";
   }
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  if(window.location.href=="http://localhost:8000/about/")
+    document.getElementById("content").innerHTML="EXPERIENCE THE ESSENCE OF BEAUTY WITH SKIN-SAVIOUR";
+  else if(window.location.href=="http://localhost:8000/quiz/")
+    document.getElementById("content").innerHTML="QUIZ";
+  else if(window.location.href=="http://localhost:8000/skin_treatment/")
+    document.getElementById("content").innerHTML="SKIN TREATMENT";
+  else if(window.location.href=="http://localhost:8000/tips/")
+    document.getElementById("content").innerHTML="TIPS";
+  else if(window.location.href=="http://localhost:8000/contact_us/")
+    document.getElementById("content").innerHTML="CONTACT US";
+});
